256dad9b957f998d1ea6fab0a707edfe0b524021
Suryansh Tilak <suryansh_tilak@epam.com>
2025-05-20 01:12:26 +0530

    c3

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -3,6 +3,7 @@ output "sql_server_fqdn" {
   value       = azurerm_mssql_server.sql_server.fully_qualified_domain_name
 }
 
+# sql_connection_string is marked as sensitive to ensure secure handling
 output "sql_connection_string" {
   description = "The ADO.NET connection string for the Azure SQL Database with SQL authentication."
   value       = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.sql_admin_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
